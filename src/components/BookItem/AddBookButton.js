30b70c9d4516511f3022ff05a3eab95b252143ff
AnnaSokoltsova <annivawork@gmail.com>
2023-05-17 18:16:35 +0300

    Implemented badge for the book added
    Implemented flow of badge display on the book added

---
+++ src/components/BookItem/AddBookButton.js
@@ -0,0 +1,14 @@
+import classes from "./BookItem.module.css";
+
+
+export default function AddBookButton({ handleAddBook}) {
+
+  return (
+    <button className={classes["single-book__btn"]} onClick={handleAddBook}>
+      <svg>
+        <rect x="0" y="0" fill="none" width="100%" height="100%" />
+      </svg>
+      Add the book
+    </button>
+  );
+}
--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,19 +1,54 @@
-import React from "react";
-import classes from "./BookItem.module.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useDispatch } from "react-redux";
+import { bookshelfActions } from "../../store/bookshelf-slice";
+import { useNavigate } from "react-router-dom";
+import { sendBookshelfData } from "../../store/bookshelf-actions";
 import missingCover from "../../images/missingcover.png";
+import Bookmark from "../Badges/Bookmark";
+import AddBookButton from "./AddBookButton";
+import classes from "./BookItem.module.css";
 
-export default function BookItem({ children, id, author, title, coverImg }) {
+export default function BookItem({ id, author, title, coverImg }) {
+  const [bookAdded, setBookAdded] = useState(false);
+  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
 
   const replaceImageOnError = (event) => {
     event.currentTarget.src = missingCover;
   };
 
+  const handleAddBook = () => {
+    
+    if (!currentUser) {
+      navigate("/signin");
+      return;
+    }
+
+    dispatch(
+      bookshelfActions.addBookToShelf({
+        id,
+        author,
+        title,
+        coverImg,
+      })
+    );
+
+    setBookAdded(true);
+
+    const userId = currentUser.uid;
+    dispatch(sendBookshelfData(id, author, title, coverImg, userId));
+  };
 
   return (
     <article className={classes["single-book"]}>
       <div className={classes["single-book__image"]}>
-      <Link to={`/${id}`}><img src={coverImg} alt="cover" onError={replaceImageOnError}/></Link>
+        <Link to={`/${id}`}>
+          <img src={coverImg} alt="cover" onError={replaceImageOnError} />
+        </Link>
       </div>
       <div className={classes["single-book__content"]}>
         <div className={classes["single-book__text-container"]}>
@@ -21,10 +56,19 @@ export default function BookItem({ children, id, author, title, coverImg }) {
             <span>{author}</span>
           </div>
           <div className={classes["single-book__text"]}>
-            <Link to={`/${id}`} className={classes["single-book__link"]}>{title}</Link>
+            <Link to={`/${id}`} className={classes["single-book__link"]}>
+              {title}
+            </Link>
           </div>
         </div>
-        {children}
+        {bookAdded && <Bookmark />}
+        <AddBookButton
+          author={author}
+          id={id}
+          title={title}
+          coverImg={coverImg}
+          handleAddBook={handleAddBook}
+        />
       </div>
     </article>
   );
